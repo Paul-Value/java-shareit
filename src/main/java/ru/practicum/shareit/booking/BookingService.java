e02ba6e10f1bc92ae2fb3a7c5e1c2502ce9246b6
Paul Value <paulvalue@gmail.com>
2024-12-09 23:43:08 +0300

    Исправление checkstyle

--- src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface BookingService {
     BookingDto save(BookingCreateDto dto);
+
     BookingDto updateStatusBooking(long bookingId, long userId, boolean approved);
 
     BookingDto get(long bookingId, long userId);
